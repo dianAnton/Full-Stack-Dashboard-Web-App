cbd4f33871b96a79b74bedd432ec8f50f66e3300
dianAnton <antonbusiness.project@gmail.com>
2025-01-18 23:43:15 -0300

    Error in route }.ts in the seed folder solve

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -100,3 +100,19 @@ async function seedRevenue() {
 
   return insertedRevenue;
 }
+
+export async function GET() {
+  try {
+    await client.sql`BEGIN`;
+    await seedUsers();
+    await seedCustomers();
+    await seedInvoices();
+    await seedRevenue();
+    await client.sql`COMMIT`;
+
+    return Response.json({ message: 'Database seeded successfully' });
+  } catch (error) {
+    await client.sql`ROLLBACK`;
+    return Response.json({ error }, { status: 500 });
+  }
+}
