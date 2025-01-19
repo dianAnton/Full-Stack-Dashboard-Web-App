5003b97fb1b682addf165465ae47c8497fa4fd55
dianAnton <antonbusiness.project@gmail.com>
2025-01-18 21:34:32 -0300

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+// You'll use this file to keep the fonts that will be used throughout your application.
+
+import { Lusitana } from 'next/font/google';
+import { Inter } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+ });
